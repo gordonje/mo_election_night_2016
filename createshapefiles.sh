e0295b6b1bc854a6d291b4db3712aee28256f661
jiayi28 <jw778@mail.missouri.edu>
2016-10-13 16:23:38 -0500

    #9Acquire shape data
    Finished #9 acquire shape data

--- createshapefiles.sh
+++ createshapefiles.sh
@@ -7,3 +7,54 @@ topojson > topojson/mo_senate_2000.topo.json \
 -q 5000 \
 -s 0.00000008 \
     house_districts=state_legislative/2000su_shp/su29_d00.shp --id-property GEOID 
+
+topojson > topojson/mo_house_2006.topo.json \
+-q 5000 \
+-s 0.00000008 \
+    house_districts=state_legislative/2006sl_shp/sl29_d11.shp --id-property GEOID 
+
+topojson > topojson/mo_senate_2006.topo.json \
+-q 5000 \
+-s 0.00000008 \
+    house_districts=state_legislative/2006su_shp/su29_d11.shp --id-property GEOID 
+
+topojson > topojson/mo_house_2010.topo.json \
+-q 5000 \
+-s 0.00000008 \
+    house_districts=state_legislative/2010sl_500k/gz_2010_29_620_l2_500k.shp --id-property GEOID 
+
+topojson > topojson/mo_senate_2010.topo.json \
+-q 5000 \
+-s 0.00000008 \
+    house_districts=state_legislative/2010su_500k/gz_2010_29_610_u2_500k.shp --id-property GEOID 
+
+topojson > topojson/mo_house_2013.topo.json \
+-q 5000 \
+-s 0.00000008 \
+    house_districts=state_legislative/2013sl_500k/cb_2013_29_sldl_500k.shp --id-property GEOID 
+
+topojson > topojson/mo_senate_2013.topo.json \
+-q 5000 \
+-s 0.00000008 \
+    house_districts=state_legislative/2013su_500k/cb_2013_29_sldu_500k.shp --id-property GEOID
+
+topojson > topojson/mo_house_2014.topo.json \
+-q 5000 \
+-s 0.00000008 \
+    house_districts=state_legislative/2014sl_500k/cb_2014_29_sldl_500k.shp --id-property GEOID 
+
+topojson > topojson/mo_senate_2014.topo.json \
+-q 5000 \
+-s 0.00000008 \
+    house_districts=state_legislative/2014su_500k/cb_2014_29_sldu_500k.shp --id-property GEOID
+ 
+topojson > topojson/mo_house_2015.topo.json \
+-q 5000 \
+-s 0.00000008 \
+    house_districts=state_legislative/2015sl_500k/cb_2015_01_sldl_500k.shp --id-property GEOID 
+
+topojson > topojson/mo_senate_2015.topo.json \
+-q 5000 \
+-s 0.00000008 \
+    house_districts=state_legislative/2015su_500k/cb_2015_29_sldu_500k.shp --id-property GEOID
+ 
