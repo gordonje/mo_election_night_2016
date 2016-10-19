fb4ef2f5ea3ed86931ed953d1e2a583be4371c19
James Gordon <gordon.je@gmail.com>
2016-10-18 23:40:46 -0500

    Add script for setting up dynamodb election results table #5

---
+++ set_up_dynamodb.py
@@ -0,0 +1,38 @@
+import boto3
+
+# create a boto3 session (should load your stored credentials automatically)
+session = boto3.Session()
+
+# create a client for interacting with dynamodb
+dynamodb = session.resource('dynamodb')
+
+table = dynamodb.create_table(
+    TableName='election_results3',
+    KeySchema=[
+                {
+            'AttributeName': 'race_type',
+            'KeyType': 'HASH' 
+        },
+        {
+            'AttributeName': 'last_updated',
+            'KeyType': 'RANGE'
+        }
+    ],
+    AttributeDefinitions=[
+        {
+            'AttributeName': 'race_type',
+            'AttributeType': 'S'
+        },
+        {
+            'AttributeName': 'last_updated',
+            'AttributeType': 'N'
+        },
+
+    ],
+    ProvisionedThroughput={
+        'ReadCapacityUnits': 20,
+        'WriteCapacityUnits': 1,
+    }
+)
+
+table.meta.client.get_waiter('table_exists').wait(TableName='election_results3')
